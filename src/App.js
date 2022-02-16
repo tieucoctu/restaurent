dfb25356bcae00fb1b1ce9fa05c2f1c3864a2e18
longtc9c2000 <65326062+longtc9c2000@users.noreply.github.com>
2022-02-17 01:30:42 +0700

    j

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-import logo from './logo.svg';
 import './App.css';
 import{Header, AboutUs,Chef, SpecialMenu, Intro, Laurels, Gallery,FindUs, Footer} from './container'
 import {Navbar} from './components'
